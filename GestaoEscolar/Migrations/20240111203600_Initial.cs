53ecf3167498581d888c8fc1f532caf2b3d99bc5
Thiago Campos <thiago-kono@hotmail.com>
2024-01-11 17:37:55 -0300

    add Migration

---
+++ GestaoEscolar/Migrations/20240111203600_Initial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestao.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Instituicao",
+                columns: table => new
+                {
+                    InstituicaoID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instituicao", x => x.InstituicaoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departamento",
+                columns: table => new
+                {
+                    DepartamentoID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fk_InstituicaoID = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamento", x => x.DepartamentoID);
+                    table.ForeignKey(
+                        name: "FK_Departamento_Instituicao_fk_InstituicaoID",
+                        column: x => x.fk_InstituicaoID,
+                        principalTable: "Instituicao",
+                        principalColumn: "InstituicaoID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departamento_fk_InstituicaoID",
+                table: "Departamento",
+                column: "fk_InstituicaoID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamento");
+
+            migrationBuilder.DropTable(
+                name: "Instituicao");
+        }
+    }
+}
